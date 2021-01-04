b7474ee7adcf9ce47d95d7f3630652722792dedd
Gonzalo Ribera <gonzalo.ribera@gmail.com>
2021-01-04 17:07:03 -0300

    Se agrega validación para no permitir hacer peticiones mientras el objeto no haya sido conectado al BUS.

--- microredes.py
+++ microredes.py
@@ -1,11 +1,24 @@
 import connection as conn
 
-class Microredes():
-	conn = None
-
+class Microredes(object):
 	def __init__(self, addr, equipo):
 		self.addr = addr
 		self.equipo = equipo
+		self.conn = conn.Connection()
 
-	def connect(port, baudrate):
-		self.conn = conn.Connection(port, baudrate)
+	def __getattribute__(self, name):
+		attr = object.__getattribute__(self, name)
+		if hasattr(attr, '__call__'):
+			def newfunc(*args, **kwargs):
+				if not self.conn.isConnected() and attr.__name__ != 'connect':
+					raise ConnectionError('No está conectado')
+
+				result = attr(*args, **kwargs)
+				return result
+			return newfunc
+		else:
+			return attr
+
+	def connect(self, port, baudrate):
+		self.conn.connect(port, baudrate)
+
